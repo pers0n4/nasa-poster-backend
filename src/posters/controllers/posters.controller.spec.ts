4e336eb9e590ebb2a479dbdb2b5ce394cde6c27f
Dong-Young Kim <31337.persona@gmail.com>
2022-03-09 02:41:54 +0900

    test: fix repository mock

--- src/posters/controllers/posters.controller.spec.ts
+++ src/posters/controllers/posters.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { PostersController } from "./posters.controller";
 import { PostersService } from "../services/posters.service";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Poster } from "../entities/poster.entity";
 
 describe("PostersController", () => {
   let controller: PostersController;
@@ -8,7 +10,15 @@ describe("PostersController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostersController],
-      providers: [PostersService],
+      providers: [
+        PostersService,
+        {
+          provide: getRepositoryToken(Poster),
+          useValue: {
+            find: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<PostersController>(PostersController);
--- src/posters/services/posters.service.spec.ts
+++ src/posters/services/posters.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Poster } from "../entities/poster.entity";
 import { PostersService } from "./posters.service";
 
 describe("PostersService", () => {
@@ -6,7 +8,15 @@ describe("PostersService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostersService],
+      providers: [
+        PostersService,
+        {
+          provide: getRepositoryToken(Poster),
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PostersService>(PostersService);

4f122a5131412bd276948c8cee1bae10208f6002
Dong-Young Kim <31337.persona@gmail.com>
2022-03-09 02:37:20 +0900

    feat: use ServeStaticModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PostersModule } from "./posters/posters.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), PostersModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "..", "static"),
+      serveRoot: "/static",
+    }),
+    PostersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
